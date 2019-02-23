7dc644bc5adb1e55eb265d1f9eab1b4704175a8c
jrmmba8314 <jrmmba@outlook.com>
2019-02-23 10:09:51 -0800

    cleaning up repo

--- restfulemps/src/main/java/com/lambdaschool/restfulemps/EmployeeController.java
+++ restfulemps/src/main/java/com/lambdaschool/restfulemps/EmployeeController.java
@@ -2,6 +2,7 @@ package com.lambdaschool.restfulemps;
 
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
+@RequestMapping(path = {}, produces = MediaType.APPLICATION_JSON_VALUE)
 @RestController
 public class EmployeeController
 {
--- restfulemps/src/main/java/com/lambdaschool/restfulemps/RestfulempsApplication.java
+++ restfulemps/src/main/java/com/lambdaschool/restfulemps/RestfulempsApplication.java
@@ -1,16 +1,38 @@
 package com.lambdaschool.restfulemps;
 
+// jrmmba - commented out to make servlet
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class RestfulempsApplication
+//{
+//
+//    public static void main(String[] args)
+//    {
+//        SpringApplication.run(RestfulempsApplication.class, args);
+//    }
+//
+//}
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestfulempsApplication
+public class RestfulempsApplication extends SpringBootServletInitializer
 {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(RestfulempsApplication.class);
+    }
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         SpringApplication.run(RestfulempsApplication.class, args);
     }
-
 }
 
