6cf5103342544ce8986fabd2a3aa7e433734b746
jrmmba8314 <jrmmba@outlook.com>
2019-05-30 20:07:15 -0700

    Rabbit MQ updates

--- mqlistenerrestfulemps/src/main/java/com/lambdaschool/webemployees/Services/MessageListener.java
+++ mqlistenerrestfulemps/src/main/java/com/lambdaschool/webemployees/Services/MessageListener.java
@@ -2,25 +2,21 @@ package com.lambdaschool.webemployees.Services;
 
 import com.lambdaschool.webemployees.WebemployeesApplication;
 import com.lambdaschool.webemployees.model.MessageDetail;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MessageListenerHigh
+public class MessageListener
 {
-    private static final Logger logger = LoggerFactory.getLogger(MessageListenerHigh.class);
-
     @RabbitListener(queues = WebemployeesApplication.QUEUE_NAME_HIGH)
     public void receiveMessage(MessageDetail msg)
     {
-        logger.info("Received message {}", msg.toString());
+        System.out.println("Received High Queue message {" + msg.toString() + "}");
     }
 
-    // @RabbitListener(queues = WebemployeesApplication.QUEUE_NAME_LOW)
+    @RabbitListener(queues = WebemployeesApplication.QUEUE_NAME_LOW)
     public void receiveLowMessage(MessageDetail msg)
     {
-        logger.info("Received message {}", msg.toString());
+        System.out.println("Received Low  Queue message {" + msg.toString() + "}");
     }
 }
